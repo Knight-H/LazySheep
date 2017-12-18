dd3e067b0b8075a276fca96c93b91c13bdb01dd1
Thanapapas Horsuwan <thanapapas@hotmail.com>
2017-12-19 02:59:07 +0700

    changed database to mlab

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //module.exports returns this when 'required'
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb://heroku_b13mg2tn:oiej8jn5vgvlb15bhl22t144k@ds053658.mlab.com:53658/heroku_b13mg2tn',
     secret: 'yoursecret'
 }
